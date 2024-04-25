cd0a0a48bc527b6fb5932a6e0ad05347d7237ba8
Alexander Podgorny <apodgorny@users.noreply.github.com>
2024-04-25 12:53:24 +0300

    update

--- poetry.sh
+++ poetry.sh
@@ -1,17 +1,11 @@
 function poetry_init {
-	# Check if Poetry is installed
-	if command -v poetry &>/dev/null; then
-		echo "Poetry is already installed."
-	else
-		echo "Installing Poetry..."
-		pip install poetry
-
-		echo "Installing poetry-exec-plugin..."
-		pip install poetry-exec-plugin
-		poetry self add poetry-exec-plugin
-
-		echo "Poetry and poetry-exec-plugin have been installed."
+	# Check if Poetry is not installed, silently
+	if ! command -v poetry &>/dev/null; then
+		pip install poetry &>/dev/null
+		pip install poetry-exec-plugin &>/dev/null
+		poetry self add poetry-exec-plugin &>/dev/null
 	fi
 }
+
 
 poetry_init()
